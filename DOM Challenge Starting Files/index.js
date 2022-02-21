90b22bff54b57276c189f95f19e4b3d18aade9ae
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-02-21 21:44:37 +0200

    more sections code

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelectorAll("li")[2].innerHTML = "3mmo Yousef";
+
+var a_tag = document.querySelector(".list a");
+a_tag.style.color = "springgreen";
+a_tag.style.fontSize = "30px";
---
+++ DOM/app.js
@@ -0,0 +1,9 @@
+// to change the content of an html element //
+
+// document.querySelector("h1").innerHTML = "GOOD BYE";
+
+// to get access to html elements //
+
+var html_head = document.firstElementChild.firstElementChild;
+var html_body = document.firstElementChild.lastElementChild;
+var h1_element = html_body.firstElementChild;
