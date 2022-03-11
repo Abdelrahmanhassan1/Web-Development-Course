6142091c2173f3287ee472b15124173f2e778415
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-03-11 15:05:09 +0200

    add EJS Code

--- EJS/ToDoList-v1/app.js
+++ EJS/ToDoList-v1/app.js
@@ -1,14 +1,55 @@
 const express = require("express");
 const body_parser = require("body-parser");
-
+const port = 3000;
 const app = express();
 
-const port = 3000;
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.send("Hello form server...");
+  var today = new Date();
+  var current_day = today.getUTCDay();
+  console.log(today);
+  var options_1 = {
+    weekday: "long",
+  };
+  var options_2 = {
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options_1);
+  var date = today.toLocaleDateString("en-US", options_2);
+  res.render("list", { OurCurrentDay: day, CurrentDate: date });
 });
 
 app.listen(port || process.env.PORT, function () {
   console.log("Now I am Listening..");
 });
+
+// const week_days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// switch (current_day) {
+//   case 0:
+//     day = week_days[0];
+//   case 1:
+//     day = week_days[1];
+//   case 2:
+//     day = week_days[2];
+//   case 3:
+//     day = week_days[3];
+//   case 4:
+//     day = week_days[4];
+//   case 5:
+//     day = week_days[5];
+//   case 6:
+//     day = week_days[6];
+// }
